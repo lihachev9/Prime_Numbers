28c1afdb5209dd7b7a4258c984f4ca100be1fc49
Андрей Лихачёв <andreyyy.lihachev@gmail.com>
2022-05-22 00:07:00 +0300

    add c++

---
+++ Prime_Numbers/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if (num == 2) return true;
+    if (num <= 1 || num % 2 == 0) return false;
+    double sqrt_num = sqrt(num);
+    for (int div = 3; div <= sqrt_num; div += 2)
+        if (num % div == 0) return false;
+    return true;
+}
+
+bool run_program(int N) {
+    bool tmp;
+    for (int i = 0; i < N; i++) tmp = isPrime(i);
+    return tmp;
+}
+
+int main() {
+    int N = 10000000;
+    clock_t start, end;
+    start = clock();
+    cout << run_program(N) << endl;
+    end = clock();
+    cout << (end - start) / ((double)CLOCKS_PER_SEC);
+    system("pause");
+    return 0;
+}
